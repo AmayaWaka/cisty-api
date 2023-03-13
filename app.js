0a525242b8ca07ef325918a505b286f1db224c2f
AmayaWaka <rmadegwa63@gmail.com>
2023-03-13 12:45:03 +0100

    Separated users and tour into different modules

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const fs = require('fs');
 const express = require("express");
 const morgan = require("morgan");
-const app = express();
 
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -17,186 +18,9 @@ app.use((req, res, next)=>{
 });
 
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-
-
-//Initializing routes
-//Route handlers
-const getAllTours = (req, res)=>{
-    console.log(req.requestTime);
-    res
-    .status(200)
-    .json({
-        status: "Success",
-        requestedAt: req.requestTime,
-        result: tours.length,
-        data: {
-            tours
-        }
-    });
-    
-    }
-
-const getTour = (req, res) => {
-    const id = req.params.id * 1;
-
-    //Getting a specific tour from an object in an array
-    //Handling an error or wrong input
-    if(id > tours.length){
-        return res.status(404)
-        .json({
-            status: "fail",
-            message: "Invalid request"
-        });
-    }
-    const tour = tours.find(el => el.id === id);
-    res.status(200)
-    .json({
-        status: "Success",
-        data:{
-            tour
-        }
-    })
-
-}
-
-const createTour = (req, res)=>{
-    const newId = tours[tours.length-1].id+1;
-    const newTour = Object.assign({id: newId}, req.body);
-
-    tours.push(newTour);
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        err=>{
-            res.status(201).json({
-                status: "Success",
-                data: {
-                    tour: newTour
-                }
-            });
-        }         
-    );
-}
-const patchTour = (req, res)=>{
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: "fail",
-            message: "Invalid Input"
-        });
-    }
-
-    res.status(200)
-    .json({
-        status: "Success",
-        data:{
-            tour: '<Updated tour here>'
-        }
-    });
-}
-
-const deleteTour = (req, res)=>{
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: "fail",
-            message: "Invalid Input"
-        });
-    }
-
-    res.status(204)
-    .json({
-        status: "Success",
-        data: null
-    });
-}
-// User handlers
-const getAllUsers = (req, res) => {
-    res
-    .status(500)
-    .json({
-        status: "error",
-        message: "This route is not yet defined"
-    });
-}
-const createUser = (req, res) => {
-    res
-    .status(500)
-    .json({
-        status: "error",
-        message: "This route is not yet defined"
-    });
-}
-
-const getUser = (req, res) => {
-    res
-    .status(500)
-    .json({
-        status: "error",
-        message: "This route is not yet defined"
-    });
-}
-
-const updateUser = (req, res) => {
-    res
-    .status(500)
-    .json({
-        status: "error",
-        message: "This route is not yet defined"
-    });
-}
-
-const deleteUser = (req, res) => {
-    res
-    .status(500)
-    .json({
-        status: "error",
-        message: "This route is not yet defined"
-    });
-}
-    //Tours handler
-
-
-
-//Routes
-
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-//Mounting our router
-app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
-
-
-tourRouter
-.route("/")
-.get(getAllTours)
-.post(createTour);
-tourRouter
-.route("/:id")
-.get(getTour)
-.patch(patchTour)
-.delete(deleteTour);
-
-
-//User routes
-//Using express router
-
-;
-
-
-
-userRouter
-.route("/")
-.get(getAllUsers)
-.post(createUser);
-
-userRouter
-.route("/:id")
-.get(getUser)
-.patch(updateUser)
-.delete(deleteUser);
-
+app.use("/api/v1/users", userRouter);
 
 //Setting up our port response
 //Start sever
