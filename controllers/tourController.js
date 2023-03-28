05098751d5446545d718c0bda3010ee09d92f860
AmayaWaka <rmadegwa63@gmail.com>
2023-03-28 19:56:36 +0100

    Added sorting by price, rating and created at

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,20 +6,28 @@ exports.getAllTours = async (req, res)=>{
  
     try{
         //Build query
-        //1) Filtering
+        //1a) Filtering
         const queryObj = {...req.query};
         excludedFields = ['page', 'sort', 'limit', 'fields'];
         //looping through the excluded fields  to delete from req.query value
 
         excludedFields.forEach(el => delete queryObj[el]);
-        //Advanced filtering
+        //1b)Advanced filtering 
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match =>`$${match}`);
-        console.log(JSON.parse(queryStr));
+       
+        let query = Tour.find(JSON.parse(queryStr));
+        //2)Result sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+            // console.log(query);
+
+        }else{
+            query = query.sort('-createdAt');
+        }
 
 
-
-        const query = Tour.find(JSON.parse(queryStr));
 
         //Executing query
         const tours = await query;
