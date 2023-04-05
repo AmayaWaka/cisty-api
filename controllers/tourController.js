336bae3d900f679e9a0fdf5c38f3813dfa6a58cf
AmayaWaka <rmadegwa63@gmail.com>
2023-04-05 07:32:35 +0100

    Added aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,7 +9,7 @@ exports.aliasTopTours = (req, res, next) => {
     next();
 
 }
-//Introducing classes to make our features more reusable
+
 
 
 //Getting tours using promises
@@ -129,4 +129,62 @@ exports.deleteTour = async (req, res)=>{
             message: err            
         });
     }    
+}
+exports.getTourStats = async (req, res) => {
+    try{
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte:4.5 } }
+            },
+            { 
+                $group: {
+                    _id: "$difficulty",
+                    numTours: { $sum: 1 },
+                    numRatings: { $sum: "$ratingsQuantity" },
+                    avgRating: { $avg: "$ratingsAverage" },
+                    avgPrice: { $avg: "$price" },
+                    minPrice: { $min: "$price" },
+                    maxPrice: { $max: "$price" }
+                }
+             },
+             {
+                $sort: { avgPrice: 1 }
+             }
+        ]);
+        res.status(200)
+        .json({
+            status: "Success",
+            data: {
+                stats
+            }
+        });        
+    }catch(err){
+        res.status(404).json({
+            status: "Failed",
+            message: err            
+        });       
+    }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+    try{
+        const year = req.params.year * 1;
+        const plan = await Tour.aggregate([]);
+
+
+        res.status(200).json({
+            status: "Success",
+            data: {
+                plan
+            }           
+        }); 
+
+
+    }catch(err){
+        res.status(404)
+        .json({
+            status: "Failed",
+            message: err
+        });
+    }
 }
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,9 @@ router
 //Getting top 5 cheap
 .route("/top-5-cheap")
 .get(tourController.aliasTopTours, tourController.getAllTours);
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 
 router
 .route("/")
