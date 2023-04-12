c38f14b5cf34b5d3cde6d02c4de3b58c2dda8698
AmayaWaka <rmadegwa63@gmail.com>
2023-04-12 15:39:51 +0300

    Separated operational and progrmming error handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,16 +5,28 @@ const sendErrorDev = (err, res)=>{
         error: err,
         message: err.message,
         stack: err.stack
-
 });
-}
+};
 //Handling production errors
 const sendErrorProd = (err, res)=>{
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-});
-}
+    //Operational, trusted error send message to client
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+    });
+    //Programming or other unknown error: Don't leak error details
+    }else{
+        //Logging the error
+        console.error("Error", err);
+        //Send generic message
+
+        res.status(500).json({
+            status: "error",
+            message: "Something went very wrong"
+        });
+    }
+};
 module.exports = (err, req, res, next)=>{
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
