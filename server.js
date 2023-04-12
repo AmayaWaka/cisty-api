ccffa00a38df64b1e3b5e78db0e0b3bc3d6307c8
AmayaWaka <rmadegwa63@gmail.com>
2023-04-12 18:08:45 +0300

    Handled rejected asyncronous promises

--- server.js
+++ server.js
@@ -6,20 +6,24 @@ const dotenv = require("dotenv");
 // const DB = process.env.DATABASE_LOCAL;
 dotenv.config({ path: "./.env" });
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/cisty').then(()=>{
-    console.log("DB Connected successfully");
-});
-
+mongoose.connect('mongodb://127.0.0.1:27017/cisty')
+.then(() => console.log("Db connected successfully"))
+.catch(err => console.log("Error"));
 
 
 const app = require('./app');
 dotenv.config({ path: "./.env" });
 
-
 // console.log(process.env);
 const port = process.env.PORT || 3000
-app.listen(port, ()=>{
+const server = app.listen(port, ()=>{
     console.log(`App running on port ${port}`);
 });
-
+//Handling unhandled rejection in asyncronous code
+process.on("unhandledRejection", err => {
+    console.log(err.name, err.message);
+    console.log("UNHANDLED REJECTION! SHUTTING DOWN");
+    server.close(() => {
+        process.exit(1);
+    });
+});
